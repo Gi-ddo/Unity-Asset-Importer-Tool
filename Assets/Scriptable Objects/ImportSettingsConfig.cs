3ec20661e4df3f3a4a9a452476efeadef2e721f4
Gi-ddo <60913804+Gi-ddo@users.noreply.github.com>
2021-02-04 23:04:53 +0200

    Scene set up
    -Added Configurable Asset Import Scriptable object
    -Added Texture and Audio clip Editor Scripts

---
+++ Assets/Scriptable Objects/ImportSettingsConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New ImportConfig", menuName ="ImportConfig")]
+public class ImportSettingsConfig : ScriptableObject
+{
+    public int MaxTextureSize;
+
+    [Range(0,9)]
+    public int AnsiotropicLevel;
+    
+    
+   
+}
